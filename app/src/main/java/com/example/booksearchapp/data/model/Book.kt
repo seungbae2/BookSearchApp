f02a74788b91d857631e74a67f8c4941bace20e7
SB <seungbae2@gmail.com>
2023-01-06 22:52:23 +0900

    Room DB setup (Entity, Dao, Database, TypeConverter)

--- app/src/main/java/com/example/booksearchapp/data/model/Book.kt
+++ app/src/main/java/com/example/booksearchapp/data/model/Book.kt
@@ -2,19 +2,25 @@ package com.example.booksearchapp.data.model
 
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 @JsonClass(generateAdapter = true)
+@Entity(tableName = "books")
 data class Book(
     val authors: List<String>,
     val contents: String,
     val datetime: String,
+    @PrimaryKey(autoGenerate = false)
     val isbn: String,
     val price: Int,
     val publisher: String,
+    @ColumnInfo(name = "sale_price")
     @field:Json(name = "sale_price")
     val salePrice: Int,
     val status: String,
